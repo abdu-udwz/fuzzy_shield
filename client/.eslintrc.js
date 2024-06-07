f00448f879670b9128aa5857849125a2954a6e4d
Abdulhamid <zoubi.abdulhamid@gmail.com>
2024-06-08 00:44:06 +0200

    chore: client linter & deps

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -9,12 +9,45 @@ module.exports = {
   env: {
     node: true,
   },
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    parser: "@typescript-eslint/parser",
+    project: true,
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ["@typescript-eslint"],
   extends: [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:vue/vue3-recommended",
+    "plugin:vuetify/base",
+    "@vue/eslint-config-typescript",
   ],
   rules: {
-    'vue/multi-word-component-names': 'off',
+    "vue/multi-word-component-names": "off",
+    "vue/require-default-prop": "off",
+    "vue/multi-word-component-names": "off",
+    "vue/html-self-closing": [
+      "error",
+      {
+        html: {
+          void: "always",
+        },
+      },
+    ],
+    "vue/multi-word-component-names": "warn",
+    "vue/no-template-shadow": "off",
+    "vuetify/no-deprecated-components": "warn",
+    "vue/component-name-in-template-casing": ["error", "PascalCase"],
+    "vue/no-boolean-default": "error",
+    "vue/no-child-content": "error",
+    "vue/no-this-in-before-route-enter": "error",
+    "vue/v-on-function-call": "error",
+    "vue/script-setup-uses-vars": "error",
+
+    "@typescript-eslint/no-unsafe-assignment": "warn",
+    "@typescript-eslint/no-unsafe-member-access": "warn",
+    "@typescript-eslint/no-explicit-any": "warn",
+    "@typescript-eslint/ban-ts-comment": "off"
   },
-}
+};
